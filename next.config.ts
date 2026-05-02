9ab3cabb6018ecc16af67ea45b80f2f5695ec453
Sunil Band <sunilband0007@gmail.com>
2026-05-02 10:55:00 +0530

    feat: add serwist pwa and react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
+import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
+import { env } from "./lib/env";
+
+const isProd = env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   turbopack: {},
   reactStrictMode: true,
+  reactCompiler: isProd,
 
   transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 
   experimental: {
+    inlineCss: isProd,
     optimizePackageImports: [
       "@phosphor-icons/react",
       "motion",
@@ -14,9 +20,24 @@ const nextConfig: NextConfig = {
     ],
   },
 
+  ...(isProd && {
+    compiler: {
+      removeConsole: {
+        exclude: ["error"],
+      },
+    },
+  }),
   logging: {
     fetches: {},
+    // browserToTerminal: true,
   },
 };
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  disable: process.env.NODE_ENV !== "production",
+  reloadOnOnline: true,
+});
+
+export default withSerwist(nextConfig);
