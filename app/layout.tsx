9cbb15f9ebf28bee77fecdfe2d6df32a780e6b27
Sunil Band <sunilband0007@gmail.com>
2026-06-15 11:24:58 +0530

    feat: gtag id in env

--- app/layout.tsx
+++ app/layout.tsx
@@ -164,7 +164,7 @@ export default function RootLayout({
           </Suspense>
         </ThemeProvider>
       </body>
-      <GoogleAnalytics gaId="G-SQJJCVP529" />
+      <GoogleAnalytics gaId={process.env.GTAG_ID || ""} />
     </html>
   );
 }
